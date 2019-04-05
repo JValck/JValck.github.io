ecf155fd79cfa30c85b8cb8561223015d0109062
JValck <jasper_de_valck@hotmail.com>
2019-04-05 12:35:11 +0200

    start scaling

--- DeVrijeBelgen/playground/assets/js/games/frigoMisterie/menu.js
+++ DeVrijeBelgen/playground/assets/js/games/frigoMisterie/menu.js
@@ -14,7 +14,11 @@ class Menu extends Phaser.Scene {
     this.gravity = 300;
     this.defaultGravity = 300;
     this.score = 0;
+    this.missers = 0;
     this.frigoInterval = 2000;
+    this.defaultFrigoInterval = 2000;
+    this.gameOver = false;
+    this.resetAfter = 0;
   }
 
   preload ()
@@ -46,22 +50,47 @@ class Menu extends Phaser.Scene {
       this.roos.y = pointer.y;
     }, this);
 
-    this.scoreText = this.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#FFF' });
+    this.scoreText = this.add.text(16, 16, 'Score: 0', { fontSize: '32px', fill: '#FFF' });
+    this.missersText = this.add.text(this.cameras.main.width-20, 16, 'Missers: 0', { fontSize: '32px', fill: '#FFF' });
 
+    this.bottom_collider = new Phaser.Physics.Arcade.Sprite(this,0,this.cameras.main.height,).setOrigin(0, 0);
+    //no idea if it is necessary
+    this.bottom_collider.width = this.cameras.main.width;
+    //add to scene
+    this.add.existing(this.bottom_collider);
+    //add to physics
+    this.physics.add.existing(this.bottom_collider,true);
+    //set the physics body width/height to accommodate your desired size
+    this.bottom_collider.body.width = this.cameras.main.width;
+
+    var button = this.add.image(this.cameras.main.width-16, 16, 'fullscreen', 0).setOrigin(1, 0).setInteractive();
+    button.on('pointerup', function () {
+        if (this.scale.isFullscreen)
+        {
+            button.setFrame(0);
+            this.scale.stopFullscreen();
+        }
+        else
+        {
+            button.setFrame(1);
+            this.scale.startFullscreen();
+        }
+    }, this);
 
     this.time.delayedCall(Math.random()*this.frigoInterval, this.spawnFrigo, [], this);
-    //this.time.delayedCall(Math.random()*this.frigoInterval, this.spawnRandom, [], this);
-
-    this.physics.world.on('worldbounds', this.outOfWorld);
+    this.time.delayedCall(Math.random()*this.frigoInterval, this.spawnRandom, [], this);
   }
 
   update() {
     this.scoreText.setText('Score: '+this.score);
-
+    this.missersText.setText('Missers: '+this.missers);
+    if(this.gameOver){
+      //this.scene.start('gameOver');
+    }
   }
 
   spawnRandom(){
-    var random = Math.floor(Math.random()*4);
+    var random = Math.floor(Math.random()*8);
     switch (random) {
       case 0:
       this.spawnBomb();
@@ -77,55 +106,70 @@ class Menu extends Phaser.Scene {
       break;
     }
     this.time.delayedCall(Math.random()*this.frigoInterval, this.spawnRandom, [], this);
+    if(this.resetAfter > 0) this.resetAfter--;
+    if(this.resetAfter <= 0){
+      this.resetGameParams();
+    }
+  }
+
+  resetGameParams(){
+    this.gravity = this.defaultGravity;
+    this.frigoInterval = this.defaultFrigoInterval;
+    this.resetAfter = 0;
   }
 
   randomSpawnX(){
     return Math.floor(Math.random()*(this.sys.game.canvas.width - 200));
   }
 
-  randomVelocityY(){
-    return -1 * (this.gravity + (Math.random()*this.gravity));
+  randomVelocityY(sprite){
+    var random = (this.gravity + (Math.random()*(this.sys.game.canvas.height/2)));
+    return (random % this.sys.game.canvas.height)*-1;
   }
 
   randomVelocityX(xPos){
-    var will = Math.random()*100;
+    var will = Math.random()*(this.gravity/3);
     return (xPos > (this.sys.game.canvas.width/2)) ? (-1*will):will;
   }
 
   spawnFrigo(){
     var x = this.randomSpawnX();
     var nieuweFrigo = this.physics.add.image(x, this.sys.game.canvas.height, 'frigo')/*.setOrigin(0,0)*/.setScale(0.15);
-    nieuweFrigo.body.setVelocity(this.randomVelocityX(x), this.randomVelocityY()).setGravity(0, this.gravity);    
+    nieuweFrigo.body.setVelocity(this.randomVelocityX(x), this.randomVelocityY(nieuweFrigo)).setGravity(0, this.gravity);
     this.physics.add.overlap(this.roos, nieuweFrigo, this.frigoGeraakt, null, this);
+    this.physics.add.overlap(this.bottom_collider, nieuweFrigo, this.outOfWorld, null, this);
   }
 
   spawnBomb(){
     var x = this.randomSpawnX();
     var nieuweFrigo = this.physics.add.image(x, this.sys.game.canvas.height, 'bom')/*.setOrigin(0,0)*/.setScale(0.5);
-    nieuweFrigo.body.setVelocity(this.randomVelocityX(x), this.randomVelocityY()).setGravity(0, this.gravity);
+    nieuweFrigo.body.setVelocity(this.randomVelocityX(x), this.randomVelocityY(nieuweFrigo)).setGravity(0, this.gravity);
     this.physics.add.overlap(this.roos, nieuweFrigo, this.bomGeraakt, null, this);
   }
 
   spawnClock(){
     var x = this.randomSpawnX();
     var nieuweFrigo = this.physics.add.image(x, this.sys.game.canvas.height, 'klok')/*.setOrigin(0,0)*/.setScale(0.5);
-    nieuweFrigo.body.setVelocity(this.randomVelocityX(x), this.randomVelocityY()).setGravity(0, this.gravity);
+    nieuweFrigo.body.setVelocity(this.randomVelocityX(x), this.randomVelocityY(nieuweFrigo)).setGravity(0, this.gravity);
     this.physics.add.overlap(this.roos, nieuweFrigo, this.klokGeraakt, null, this);
   }
 
   spawnBox(){
     var x = this.randomSpawnX();
     var nieuweFrigo = this.physics.add.image(x, this.sys.game.canvas.height, 'box')/*.setOrigin(0,0)*/.setScale(0.5);
-    nieuweFrigo.body.setVelocity(this.randomVelocityX(x), this.randomVelocityY()).setGravity(0, this.gravity);
+    nieuweFrigo.body.setVelocity(this.randomVelocityX(x), this.randomVelocityY(nieuweFrigo)).setGravity(0, this.gravity);
     this.physics.add.overlap(this.roos, nieuweFrigo, this.boxGeraakt, null, this);
   }
 
   bomGeraakt(roos, bom) {
     bom.disableBody(true, true);
+    this.gameOver = true;
   }
 
   klokGeraakt(roos, klok) {
     klok.disableBody(true, true);
+    this.resetAfter += 10;
+    this.gravity = this.gravity/2;
   }
 
   frigoGeraakt(roos, frigo) {
@@ -135,11 +179,31 @@ class Menu extends Phaser.Scene {
 
   boxGeraakt(roos, box){
     box.disableBody(true, true);
-    this.score += 10;
+    if(Math.random() > 0.5){
+      this.resetGameParams();
+      this.score += 10;
+    }else{
+      this.frigoInterval -= 350;
+      this.resetAfter += 5
+      console.log("frigo");
+    }
+
   }
 
-  outOfWorld(){
-    console.log("fire");
+  outOfWorld(bottom, frigo){
+    if(frigo.body.velocity.y > 0){//moving down
+      var output;
+      var topLeft = frigo.getTopLeft(output).y;
+      var topRight = frigo.getTopRight(output).y;
+      var lastVisibleCorner = (topLeft > topRight) ? topLeft : topRight;
+      if(this.sys.game.canvas.height < lastVisibleCorner){
+        frigo.disableBody(true, true);
+        this.missers++;
+        if(this.missers === 3){
+          this.gameOver = true;
+        }
+      }
+    }
   }
 
 }
